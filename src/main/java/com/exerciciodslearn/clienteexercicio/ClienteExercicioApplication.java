7631d5bc37696b8c7eb1ae6bfadfd363292735a5
Lucas Rocha <lucasrocharsx@outlook.com>
2022-05-14 07:55:43 -0300

    First Commit Project

---
+++ src/main/java/com/exerciciodslearn/clienteexercicio/ClienteExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.exerciciodslearn.clienteexercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exerciciodslearn/clienteexercicio/ClienteExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exerciciodslearn.clienteexercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
